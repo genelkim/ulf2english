fad8a7dbfde64f3b1cc4820032d7b67f66e3b550
Gene Kim <gkim21@cs.rochester.edu>
2019-02-04 13:50:32 -0500

    Added a macro for simplifying the test definitions and put my ulf translations into these definitions.

--- test/blocks-world-qa.lisp
+++ test/blocks-world-qa.lisp
@@ -6,139 +6,13 @@
 (setq *print-failures* t)
 (setq *print-summary* t)
 
-(defparameter *gene-sentence-ulf-pairs*
-  '(("Is the Nvidia block to the left of the Texaco block?"
-     (((pres be.v) (the.d (|Nvidia| block.n)) 
-       (to.p (the.d (left-of.n (the.d |Texaco| block.n))))) ?))
-    ("Is the McDonalds block on top of the SRI block?"
-     (((pres be.v) (the.d (|McDonalds| block.n))
-       (on.p ({the}.d (top-of.n (the.d (|SRI| block.n)))))) ?))
-    ("Is the Starbucks block near the Toyota block?"
-     (((pres be.v) (the.d (|Starbucks| block.n))
-       (near.p (the.d (|Toyota| block.n)))) ?))
-    ("Is the Toyota block between the Nvidia block and the Target block?"
-     (((pres be.v) (the.d (|Toyota| block.n))
-       (between.p ((the.d (|Nvidia| block.n)) and.cc 
-                   (the.d (|Target| block.n))))) ?))
-    ("Is the SRI block fully on top of any red block?"
-     (((pres be.v) (the.d (|SRI| block.n))
-       (fully.mod-a (on.p ({the}.d (top-of.n (any.d (red.a block.n))))))) ?))
-    ("Does the Toyota block face the Nvidia block?"
-     (((pres do.aux-s) (the.d (|Toyota| block.n))
-       (face.v (the.d (|Nvidia| block.n)))) ?))
-    ("Does the tallest stack have a red block on top?"
-     (((pres do.aux-s) (the.d ((most tall.a) stack.n))
-       (have.v (a.d (red.a block.n)) (on.p ({the}.d (top-of.n *ref}))))) ?))
-    ("Is the Toyota block a part of some stack?"
-     (((pres be.v) (the.d (|Toyota| block.n))
-       (= (a.d (part-of.n (some.d stack.n))))) ?))
-    ("Is the Target block a part of some row?"
-     (((pres be.v) (the.d (|Target| block.n))
-       (= (a.d (part-of.n (some.d row.n))))) ?))
-    ("Does any row contain a red block?"
-     (((pres do.aux-s) (any.d row.n)
-       (contain.v (a.d (red.a block.n)))) ?))
-    ("Are any two green blocks touching?"
-     (((pres prog) (any.d (two.a (green.a (plur block.n)))) 
-       touch.v) ?))
-    ("Are any two stacks near each other?"
-     (((pres be.v) (any.d (two.a (plur stack.n)))
-       (near.p (each.d (other.n {ref}.n)))) ?)) ;; maybe we can just write each_other.pro?
-    ("Does the SRI block have anything on it?"
-     (((pres do.aux-s) (the.d (|SRI| block.n))
-       (have.v anything.pro (on.p it.pro))) ?))
-    ;; TODO: update the formula below once we finalize how to deal with this "blocks" 
-    ("Are the Nvidia and the SRI blocks in the same stack?"
-     (((pres be.v) (the.d ((|Nvidia| and.cc |SRI|) (plur block.n)))
-       (in.p (the.d (same.a stack.n)))) ?))
-    ("Is the Toyota block below the Texaco block?"
-     (((pres be.v) (the.d (|Toyota| block.n))
-       (below.p (the.d (|Texaco| block.n)))) ?))
-    ("Is the Starbucks block on top of any row?"
-     (((pres be.v) (the.d (|Starbucks| block.n))
-       (on.p ({the}.d (top-of.n (any.d row.n))))) ?))
-    ;; TODO: the formula below doesn't seem right since two separate 'plur'
-    ;; operators seems to preclude having one Nvidia and one McDonalds block
-    ("Are the Nvidia and the McDonalds blocks side by side?" 
-     (((pres be.v) ((the.d (|Nvidia| (plur {block}.n))) and.cc
-                    (the.d (|McDonalds| (plur block.n))))
-       side_by_side.a) ?)) 
-    ;; side_by_side.a 
-    ;; == (lambda x (all y [y (member-of.n x)] 
-    ;;                (exists z [[z (member-of.n x)] ^ (not [z = y])]
-    ;;                  (exists a [a (side-of.n y)]
-    ;;                    (exists b [b (side-of.n z)]
-    ;;                      [a by.p b])))))
-    ;; Or something like this... Too complicated to break down in ULF.
-    ("Are all the red blocks near the Nvidia block?"
-     (((pres be.v) (all.d ({of}.p (the.d (red.a (plur block.n)))))
-       (near.p (the.d (|Nvidia| block.n)))) ?))
-    ("Is every blue block behind some other block?"
-     (((pres be.v) (every.d (blue.a block.n))
-       (behind.p (some.d (other.a block.n)))) ?))
-    ("Is the Texaco block between any two blocks?"
-     (((pres be.v) (the.d (|Texaco| block.n))
-       (between.p (any.d (two.a (plur block.n))))) ?))
-    ("Is the Target block slightly to the left of some red block?"
-     (((pres be.v) (the.d (|Target| block.n))
-       (slightly.mod-a 
-        (to.p (the.d (left-of.n (some.d (red.a block.n))))))) ?))
-    ;; TODO: update the formula below once we finalize how to deal with this "blocks" 
-    ("Is there a block between the McDonalds and the SRI blocks?"
-     (((pres be.v) there.pro 
-       (a.d (n+preds block.n
-                     (between.p ((the.d (|McDonalds| (plur {block}.n))) and.cc
-                                 (the.d (|SRI| (plur block.n)))))))) ?))
-    ("Is there a block near the Target block?"
-     (((pres be.v) there.pro 
-       (a.d (n+preds block.n
-                     (near.p (the.d (|Target| block.n)))))) ?))
-    ("Is there a red block near a blue block?"
-     (((pres be.v) there.pro
-       (a.d (n+preds (red.a block.n)
-                     (near.p (a.d (blue.a block.n)))))) ?))
-    ("Is there a block touching the Nvidia block?"
-     (((pres be.v) there.pro
-       (a.d (n+preds block.n
-                     (touch.v (the.d (|Nvidia| block.n)))))) ?))
-    ("Is there anything at the front left corner of the table?"
-     (((pres be.v) there.pro
-       (any.d (n+preds thing.n
-                       (at.p (the.d (front.a (left.a 
-                                (corner-of.n (the.d table.n))))))))) ?))
-    ("Is there a row consisting of three blocks?"
-     (((pres be.v) there.pro
-       (a.d (n+preds row.n
-                     (consist.v (of.p-arg (three.d (plur block.n))))))) ?))
-    ))
-
-(defparameter *georgiy-sentence-ulf-pairs*
-  nil)
-
-(defparameter *viet-sentence-ulf-pairs*
-  nil)
-
-(defparameter *all-sentence-ulf-pairs*
-  (append *gene-sentence-ulf-pairs*
-          *georgiy-sentence-ulf-pairs*
-          *viet-sentence-ulf-pairs*))
-
-;; 1. Is the Nvidia block to the left of the Texaco block?
-(define-test bwqa-1
-  "Blocks world QA sentence 1."
-  (:tag :blocks-world-qa)
-  (let ((ulf '(((pres be.v) (the.d (|Nvidia| block.n)) 
-                (to.p (the.d ((adv-a left.a) (of.p (the.d |Texaco| block.n)))))) ?))
-        (reduced-ulf 
-             '(((pres be.v) (the.d (|Nvidia| block.n))
-                (to_the_left_of.p (the.d (|Texaco| block.n)))) ?))
-        (expected "Is the Nvidia block to the left of the Texaco block?"))
-    ;; assert-equal takes the expected value, the actual value, then values to print out on failure.
-    (assert-equal expected (ulf2english ulf) 
-                  expected (ulf2english ulf) ulf)
-    ;; Same test for reduced ULF.
-    (assert-equal expected (ulf2english reduced-ulf) 
-                  expected (ulf2english reduced-ulf) reduced-ulf)))
+;; Macro for defining tests with less writing.
+(defmacro gen-blocks-qa-test (name sentence ulf)
+  `(define-test ,name 
+      ,(format nil "Blocks world QA sentence: ~s" sentence)
+      (:tag :blocks-world-qa)
+      (assert-equal ,sentence (ulf2english ,ulf)
+                    ,sentence (ulf2english ,ulf) ,ulf)))
 
 ;;
 ;; Gene
@@ -171,6 +45,138 @@
 ;; Is there a block touching the Nvidia block?
 ;; Is there anything at the front left corner of the table?
 ;; Is there a row consisting of three blocks?
+
+(gen-blocks-qa-test bwqa-1
+  "Is the Nvidia block to the left of the Texaco block?"
+  '(((pres be.v) (the.d (|Nvidia| block.n)) 
+     (to.p (the.d (left-of.n (the.d |Texaco| block.n))))) ?))
+(gen-blocks-qa-test bwqa-2
+  "Is the McDonalds block on top of the SRI block?"
+  '(((pres be.v) (the.d (|McDonalds| block.n))
+     (on.p ({the}.d (top-of.n (the.d (|SRI| block.n)))))) ?))
+(gen-blocks-qa-test bwqa-3
+  "Is the Starbucks block near the Toyota block?"
+  '(((pres be.v) (the.d (|Starbucks| block.n))
+     (near.p (the.d (|Toyota| block.n)))) ?))
+(gen-blocks-qa-test bwqa-4
+  "Is the Toyota block between the Nvidia block and the Target block?"
+  '(((pres be.v) (the.d (|Toyota| block.n))
+     (between.p ((the.d (|Nvidia| block.n)) and.cc 
+                 (the.d (|Target| block.n))))) ?))
+(gen-blocks-qa-test bwqa-5
+  "Is the SRI block fully on top of any red block?"
+  '(((pres be.v) (the.d (|SRI| block.n))
+     (fully.mod-a (on.p ({the}.d (top-of.n (any.d (red.a block.n))))))) ?))
+(gen-blocks-qa-test bwqa-6
+  "Does the Toyota block face the Nvidia block?"
+  '(((pres do.aux-s) (the.d (|Toyota| block.n))
+     (face.v (the.d (|Nvidia| block.n)))) ?))
+(gen-blocks-qa-test bwqa-7
+  "Does the tallest stack have a red block on top?"
+  '(((pres do.aux-s) (the.d ((most tall.a) stack.n))
+     (have.v (a.d (red.a block.n)) (on.p ({the}.d (top-of.n *ref}))))) ?))
+(gen-blocks-qa-test bwqa-8
+  "Is the Toyota block a part of some stack?"
+  '(((pres be.v) (the.d (|Toyota| block.n))
+     (= (a.d (part-of.n (some.d stack.n))))) ?))
+(gen-blocks-qa-test bwqa-9
+  "Is the Target block a part of some row?"
+  '(((pres be.v) (the.d (|Target| block.n))
+     (= (a.d (part-of.n (some.d row.n))))) ?))
+(gen-blocks-qa-test bwqa-10
+  "Does any row contain a red block?"
+  '(((pres do.aux-s) (any.d row.n)
+     (contain.v (a.d (red.a block.n)))) ?))
+(gen-blocks-qa-test bwqa-11
+  "Are any two green blocks touching?"
+  '(((pres prog) (any.d (two.a (green.a (plur block.n)))) 
+     touch.v) ?))
+(gen-blocks-qa-test bwqa-12
+  "Are any two stacks near each other?"
+  '(((pres be.v) (any.d (two.a (plur stack.n)))
+     (near.p (each.d (other.n {ref}.n)))) ?)) ;; maybe we can just write each_other.pro?
+(gen-blocks-qa-test bwqa-13
+  "Does the SRI block have anything on it?"
+  '(((pres do.aux-s) (the.d (|SRI| block.n))
+     (have.v anything.pro (on.p it.pro))) ?))
+    ;; TODO: update the formula below once we finalize how to deal with this "blocks" 
+(gen-blocks-qa-test bwqa-14
+  "Are the Nvidia and the SRI blocks in the same stack?"
+  '(((pres be.v) (the.d ((|Nvidia| and.cc |SRI|) (plur block.n)))
+     (in.p (the.d (same.a stack.n)))) ?))
+(gen-blocks-qa-test bwqa-15
+  "Is the Toyota block below the Texaco block?"
+  '(((pres be.v) (the.d (|Toyota| block.n))
+     (below.p (the.d (|Texaco| block.n)))) ?))
+(gen-blocks-qa-test bwqa-16
+  "Is the Starbucks block on top of any row?"
+  '(((pres be.v) (the.d (|Starbucks| block.n))
+     (on.p ({the}.d (top-of.n (any.d row.n))))) ?))
+    ;; TODO: the formula below doesn't seem right since two separate 'plur'
+    ;; operators seems to preclude having one Nvidia and one McDonalds block
+(gen-blocks-qa-test bwqa-17
+  "Are the Nvidia and the McDonalds blocks side by side?" 
+  '(((pres be.v) ((the.d (|Nvidia| (plur {block}.n))) and.cc
+                  (the.d (|McDonalds| (plur block.n))))
+     side_by_side.a) ?)) 
+    ;; side_by_side.a 
+    ;; == (lambda x (all y [y (member-of.n x)] 
+    ;;            (exists z [[z (member-of.n x)] ^ (not [z = y])]
+    ;;              (exists a [a (side-of.n y)]
+    ;;                (exists b [b (side-of.n z)]
+    ;;                [a by.p b])))))
+    ;; Or something like this... Too complicated to break down in ULF.
+(gen-blocks-qa-test bwqa-18
+  "Are all the red blocks near the Nvidia block?"
+  '(((pres be.v) (all.d ({of}.p (the.d (red.a (plur block.n)))))
+     (near.p (the.d (|Nvidia| block.n)))) ?))
+(gen-blocks-qa-test bwqa-19
+  "Is every blue block behind some other block?"
+  '(((pres be.v) (every.d (blue.a block.n))
+     (behind.p (some.d (other.a block.n)))) ?))
+(gen-blocks-qa-test bwqa-20
+  "Is the Texaco block between any two blocks?"
+  '(((pres be.v) (the.d (|Texaco| block.n))
+     (between.p (any.d (two.a (plur block.n))))) ?))
+(gen-blocks-qa-test bwqa-21
+  "Is the Target block slightly to the left of some red block?"
+  '(((pres be.v) (the.d (|Target| block.n))
+     (slightly.mod-a 
+      (to.p (the.d (left-of.n (some.d (red.a block.n))))))) ?))
+(gen-blocks-qa-test bwqa-22
+    ;; TODO: update the formula below once we finalize how to deal with this "blocks" 
+  "Is there a block between the McDonalds and the SRI blocks?"
+  '(((pres be.v) there.pro 
+     (a.d (n+preds block.n
+                   (between.p ((the.d (|McDonalds| (plur {block}.n))) and.cc
+                               (the.d (|SRI| (plur block.n)))))))) ?))
+(gen-blocks-qa-test bwqa-23
+  "Is there a block near the Target block?"
+  '(((pres be.v) there.pro 
+     (a.d (n+preds block.n
+                   (near.p (the.d (|Target| block.n)))))) ?))
+(gen-blocks-qa-test bwqa-24
+  "Is there a red block near a blue block?"
+  '(((pres be.v) there.pro
+     (a.d (n+preds (red.a block.n)
+                   (near.p (a.d (blue.a block.n)))))) ?))
+(gen-blocks-qa-test bwqa-25
+  "Is there a block touching the Nvidia block?"
+  '(((pres be.v) there.pro
+     (a.d (n+preds block.n
+                   (touch.v (the.d (|Nvidia| block.n)))))) ?))
+(gen-blocks-qa-test bwqa-26
+  "Is there anything at the front left corner of the table?"
+  '(((pres be.v) there.pro
+     (any.d (n+preds thing.n
+                     (at.p (the.d (front.a (left.a 
+                             (corner-of.n (the.d table.n))))))))) ?))
+(gen-blocks-qa-test bwqa-27
+  "Is there a row consisting of three blocks?"
+  '(((pres be.v) there.pro
+     (a.d (n+preds row.n
+                   (consist.v (of.p-arg (three.d (plur block.n))))))) ?))
+  
 
 ;;
 ;; Viet
