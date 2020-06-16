55f1ea6af525d4fac6308a3d9095ba47de10231b
Gene Kim <gkim21@cs.rochester.edu>
2020-06-16 03:49:04 -0700

    More tracing.

--- run-coveralls.lisp
+++ run-coveralls.lisp
@@ -1,5 +1,5 @@
 (ql:quickload :cl-coveralls)
 ;(setf uiop:*temporary-directory* *default-pathname-defaults*)
-(trace dex:post dex:request coveralls::calc-coverage coveralls::get-coverage coveralls::finalize-coverage coveralls::parse-report-files string-downcase)
+(trace dex:post dex:request coveralls::calc-coverage coveralls::get-coverage coveralls::finalize-coverage coveralls::parse-report-files string-downcase usocket::socket-connect dex::ensure-socks5-connected quri::uri quri::url-encoder-params dex::make-new-connection)
 (coveralls:with-coveralls ()
   (load "runtest"))
