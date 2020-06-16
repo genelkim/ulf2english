758c8ce59bd95346dd807552292c8c1eebd282fd
Gene Kim <gkim21@cs.rochester.edu>
2020-06-15 19:48:15 -0700

    Fixed lisp dependency install script.

--- script/install_lisp_dependencies.sh
+++ script/install_lisp_dependencies.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-git clone https://github.com/genelkim/ttt.git ~/quicklisp/local-projects/
-git clone https://github.com/genelkim/cl-util.git ~/quicklisp/local-projects/
-git clone https://github.com/genelkim/ulf-lib.git ~/quicklisp/local-projects/
+git clone https://github.com/genelkim/ttt.git ~/quicklisp/local-projects/ttt
+git clone https://github.com/genelkim/cl-util.git ~/quicklisp/local-projects/cl-util
+git clone https://github.com/genelkim/ulf-lib.git ~/quicklisp/local-projects/ulf-lib
 
 CURDIR=$(pwd)
 cd ~/quicklisp/local-projects/ttt/src
