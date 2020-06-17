4a95c03e2cce329308f203b3faafbd6870b82744
Gene Kim <gkim21@cs.rochester.edu>
2020-06-16 21:54:07 -0700

    Removed all coveralls dependencies.

--- script/install_lisp_dependencies.sh
+++ script/install_lisp_dependencies.sh
@@ -5,16 +5,10 @@ set -e
 git clone https://github.com/genelkim/ttt.git ~/quicklisp/local-projects/ttt
 git clone https://github.com/genelkim/cl-util.git ~/quicklisp/local-projects/cl-util
 git clone https://github.com/genelkim/ulf-lib.git ~/quicklisp/local-projects/ulf-lib
-cp -r cl-coveralls ~/quicklisp/local-projects/cl-coveralls
 
+# Pre-load TTT to compile it.
 CURDIR=$(pwd)
 cd ~/quicklisp/local-projects/ttt/src
 cl -e '(load "load")'
 cd $CURDIR
-
-## Update quicklisp asdf version.
-#ASDF_URL="https://common-lisp.net/project/asdf/archives/asdf.lisp"
-#wget $ASDF_URL
-#rm ~/quicklisp/asdf.lisp
-#mv asdf.lisp ~/quicklisp/asdf.lisp
 
